592b88c3668d51954864845aca455134ed4b0587
Witt Sparks <wittsparks@yahoo.com>
2014-07-14 07:50:27 -0500

    Add files

---
+++ feedback.php
@@ -0,0 +1,21 @@
+<html>
+   <head>
+        <meta charset="utf-8">
+        <title>Outline Creator</title>
+        <style>
+           .title {
+              font-family:arial;
+              color:darkblue;
+              font-size:20px;
+           }
+        </style>
+   </head>
+   <body>
+   <p class="title">Thank You</p>
+   <a href="http://tree-builder.com">Back to Tree Builder</a>
+   </body>
+</html>
+<?php
+$message = $_POST['comment']."<br/><br/>From:".$_POST['email'];
+mail('wittsparks@yahoo.com', 'Comment from Tree Builder', $message);
+?>
